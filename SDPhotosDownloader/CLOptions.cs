ab581218baf5f77136d71325d747da517eeb01b6
d0nncha <d0nncha@yandex.ru>
2024-05-16 19:43:54 +0300

    -- I have allocated the definition of command line options to a separate class

---
+++ SDPhotosDownloader/CLOptions.cs
@@ -0,0 +1,16 @@
+﻿using System.CommandLine;
+
+namespace SDPhotosDownloader;
+
+public static class CLOptions
+{
+    public static Option<string> SrcOption = new Option<string>(
+        "--src", 
+        "Full path where stored raw photos on sd-card");
+    public static Option<string> DestOption = new Option<string>(
+        "--dest", 
+        "Destination root folder for raw photos");
+    public static Option<DateTime?> DateOption = new Option<DateTime?>(
+        "--date", 
+        "The date from which the photos should be uploaded");
+}
--- SDPhotosDownloader/Program.cs
+++ SDPhotosDownloader/Program.cs
@@ -2,25 +2,17 @@
 using System.CommandLine;
 using SDPhotosDownloader;
 
-var srcOption = new Option<string>(
-    "--src", 
-    "Full path where stored raw photos on sd-card");
-var destOption = new Option<string>(
-    "--dest", 
-    "Destination root folder for raw photos");
-var dateOption = new Option<DateTime?>(
-    "--date", 
-    "The date from which the photos should be uploaded");
-
-var rootCommand = new RootCommand("Available commands for SDPhotosDownloader:");
-rootCommand.AddOption(srcOption);
-rootCommand.AddOption(destOption);
-rootCommand.AddOption(dateOption);
+var rootCommand = new RootCommand("Available commands for SDPhotosDownloader:")
+{
+    CLOptions.SrcOption,
+    CLOptions.DestOption,
+    CLOptions.DateOption
+};
 
 rootCommand.SetHandler(
     Downloader.Start, 
-    srcOption, 
-    destOption, 
-    dateOption);
+    CLOptions.SrcOption, 
+    CLOptions.DestOption, 
+    CLOptions.DateOption);
 
 return rootCommand.Invoke(args);
