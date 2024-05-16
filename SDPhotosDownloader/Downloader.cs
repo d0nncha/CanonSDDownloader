05667e8a67aafd1cc1ea975490878b39cfc4c6c7
d0nncha <d0nncha@yandex.ru>
2024-05-16 19:01:05 +0300

    -- add System.CommandLine -- add command line parameters -- add Downloader.cs

---
+++ SDPhotosDownloader/Downloader.cs
@@ -0,0 +1,10 @@
+﻿namespace SDPhotosDownloader;
+
+public static class Downloader
+{
+    private const string folderFormat = "yyyy";
+    public static void Start(string src, string dest, DateTime? date)
+    {
+        
+    }
+}
---
+++ SDPhotosDownloader/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.CommandLine;
+using SDPhotosDownloader;
+
+var srcOption = new Option<string>(
+    "--src", 
+    "Full path where stored raw photos on sd-card");
+var destOption = new Option<string>(
+    "--dest", 
+    "Destination root folder for raw photos");
+var dateOption = new Option<DateTime?>(
+    "--date", 
+    "The date from which the photos should be uploaded");
+
+var rootCommand = new RootCommand("Available commands for SDPhotosDownloader:");
+rootCommand.AddOption(srcOption);
+rootCommand.AddOption(destOption);
+rootCommand.AddOption(dateOption);
+
+rootCommand.SetHandler(
+    Downloader.Start, 
+    srcOption, 
+    destOption, 
+    dateOption);
+
+return rootCommand.Invoke(args);
